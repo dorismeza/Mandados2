1fec164dec8acd669de92fb2087d25360057ee2e
Lesvin Josue <lardon99@.com>
2022-04-06 09:35:23 -0600

    prueba commit Lesvin

--- src/rutas/index.js
+++ src/rutas/index.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
-const router = Router();
 const AppControler = require('../controladores/controladorApp');
-
+const controladorProductos = require('../controladores/controladorProductos');
+const router = Router();
 
 router.get('/', AppControler.index);
 router.get('/login', AppControler.login);
 router.get('/tienda', AppControler.tienda);
+router.get('/productos', controladorProductos.listarproductos); //hola
+router.get('/comercios', AppControler.comercios); 
+
 
 module.exports= router;
